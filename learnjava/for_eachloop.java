8dbd46bd3c8bc756c6ce7f83337d02c728a71612
sacquiatenjosh <joshuabsacquiaten@gmail.com>
2025-05-09 07:55:46 +0800

    Add files via upload

---
+++ learnjava/for_eachloop.java
@@ -0,0 +1,14 @@
+package learnjava;
+
+public class for_eachloop {
+    
+    public static void main(String[] args) {
+        
+    String[] names = {"Taro", "Nagumo", "Shiba", "Shin"};
+
+    for(String name : names){
+        System.out.println(name);
+    }
+
+    }
+}
